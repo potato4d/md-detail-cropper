c895eb402e101dec77da8d86cd4db019b48321f5
Takuma HANATANI <mail@potato4d.me>
2023-12-31 17:28:24 +0900

    feat: Implement base

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+export const ROUND_SIZE = 12;
+export const BASE_SIZE = {
+  width: 2560,
+  height: 1440,
+  crop: {
+    left: 176,
+    top: 224,
+    width: 596,
+    height: 868
+  }
+} as const;
